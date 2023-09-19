8881f6bc578c35102b7f435f58e1168a0dd79d00
PaulinaMS2 <paulinasaldarriaga34@gmail.com>
2023-09-19 15:01:08 -0500

    feat: Frontend inicio de sesión

--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,52 @@
 <template>
-  <div>
-    <center>
-      <h1>Bienvenidos a la app de tareas</h1>
-      <p>Esta es una aplicación que permite gestionar las tareas de los estudiantes</p>
+  <div class="login">
+      <h1>Bienvenidos a la app de Tareas</h1>
+      <br>
+      <v-sheet width="350" class="mx-auto">
+          <v-form fast-fail @submit.prevent>
+          <v-text-field
+              label="Email"
+              prepend-icon="mdi-email"
+              type="email"
+             
+          ></v-text-field>
 
-      <a href="/tareas">Click para iniciar (sin nuxtLink)</a>
-<br>
-<br>
-      <NuxtLink to="/tareas">
-        Click para iniciar (Con nuxtLink)
-      </NuxtLink>
-    </center>
+          <v-text-field
+              label="Contraseña"
+              type="password"
+              prepend-icon="mdi-lock"
+          ></v-text-field>
+          ¿No tienes una cuenta? <nuxt-link to="/registro">Regístrate</nuxt-link>
+          <v-btn color="primary" type="submit" block class="mt-2" to="/tareas">Iniciar sesión</v-btn>
+          
+          </v-form>
+          
+      </v-sheet>
 
   </div>
+
 </template>
+
+<style>
+
+.login{
+  width: 600px;
+  margin:100px auto;
+  padding: 40px;
+  border-radius: 10px;
+  box-shadow: 5px 5px 5px 5px rgba(54, 0, 59, 0.521) ;
+  background-color: rgb(255, 255, 255);
+  text-align: center;
+  line-height: 1.5;
+}
+
+.login h1{
+  margin: 0;
+}
+
+
+</style>
+
 <script>
 definePageMeta({
   layout: "blank",
